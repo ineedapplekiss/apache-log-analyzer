3cdf53dbeb85359bd033d539270875b6fabf9dcc
shang <9212219@163.com>
2013-06-24 18:27:46 +0800

    asdfasdf

--- stream.php
+++ stream.php
@@ -1,9 +1,37 @@
 <?php
+
+
 m();
-$in = '13';
+
+/* Define our filter class */
+class strtoupper_filter extends php_user_filter {
+  function filter($in, $out, &$consumed, $closing)
+  {
+    while ($bucket = stream_bucket_make_writeable($in)) {
+      $bucket->data = strtoupper($bucket->data);
+      $consumed += $bucket->datalen;
+      stream_bucket_append($out, $bucket);
+    }
+    return PSFS_PASS_ON;
+  }
+}
+
+/* Register our filter with PHP */
+stream_filter_register("myfilter", "strtoupper_filter")
+    or die("Failed to register filter");
+
+
+
+
+$in = 'log/130621.mall.jiaju.sina.com.cn_3221.cn.gz';
 $out = '13out';
-$r = fopen($in,'r');
-$w = fopen($out,'a+');
+$r = gzopen($in,'r');
+$w = fopen($out,'w');
+
+stream_filter_append($w, "myfilter");
+
+
+
 echo stream_copy_to_stream($r,$w)."\n";
 
 m();
